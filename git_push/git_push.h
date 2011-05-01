79780e8aecbee6bc5e2dc1d9465a3d88198eb991
Watson <watson1978@gmail.com>
2011-05-02 06:19:52 +0900

    add Git push plugin

---
+++ git_push/git_push.h
@@ -0,0 +1,19 @@
+//
+//  git_push.h
+//  git_push
+//
+//  Created by Watson on 11/05/02.
+//  Copyright (c) 2011 __MyCompanyName__, All Rights Reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Automator/AMBundleAction.h>
+
+@interface git_push : AMBundleAction 
+{
+	IBOutlet id repository;
+}
+
+- (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo;
+
+@end
---
+++ git_push/git_push.m
@@ -0,0 +1,41 @@
+//
+//  git_push.m
+//  git_push
+//
+//  Created by Watson on 11/05/02.
+//  Copyright (c) 2011 Watson, All Rights Reserved.
+//
+
+#import "git_push.h"
+
+
+@implementation git_push
+
+- (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo
+{
+    NSEnumerator *enumerate = [input objectEnumerator];
+    NSString *path = [enumerate nextObject];
+    NSString *repo = [repository stringValue];
+    NSString *cmd  = [NSString stringWithFormat: @"PATH=/opt/local/bin:/usr/local/bin:/usr/local/git/bin:/usr/bin; git push %@", repo];
+
+    NSTask *task  = [[NSTask alloc] init];
+    NSPipe *pipe  = [[NSPipe alloc] init];
+
+    [task setLaunchPath: @"/bin/sh"];
+    [task setCurrentDirectoryPath: path];
+    [task setArguments: [NSArray arrayWithObjects: @"-c", cmd, nil]];
+
+    [task setStandardError: pipe];
+    [task launch];
+
+    NSFileHandle *handle = [pipe fileHandleForReading];
+    NSData *data = [handle  readDataToEndOfFile];
+    NSString *result = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+
+    [task release];
+    [pipe release];
+
+    return input;
+}
+
+@end
