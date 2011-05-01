91677bb607204d510fcf56665d625473fc9eed5f
Watson <watson1978@gmail.com>
2011-05-02 08:44:53 +0900

    add a Git pull plugin

---
+++ git_pull/git_pull.h
@@ -0,0 +1,20 @@
+//
+//  git_pull.h
+//  git_pull
+//
+//  Created by Watson on 11/05/02.
+//  Copyright (c) 2011 __MyCompanyName__, All Rights Reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Automator/AMBundleAction.h>
+
+@interface git_pull : AMBundleAction 
+{
+	IBOutlet id repository;
+	IBOutlet id rebase;
+}
+
+- (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo;
+
+@end
---
+++ git_pull/git_pull.m
@@ -0,0 +1,50 @@
+//
+//  git_pull.m
+//  git_pull
+//
+//  Created by Watson on 11/05/02.
+//  Copyright (c) 2011 __MyCompanyName__, All Rights Reserved.
+//
+
+#import "git_pull.h"
+
+
+@implementation git_pull
+
+- (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo
+{
+    NSEnumerator *enumerate = [input objectEnumerator];
+    NSString *path = [enumerate nextObject];
+    NSString *repo = [repository stringValue];
+	NSString *opt;
+	if ([rebase state] == NSOnState) {
+		opt = [[NSString alloc] initWithString:@"--rebase"];
+	} else {
+		opt = [[NSString alloc] initWithString:@""];
+	}
+
+    NSString *cmd  = [NSString stringWithFormat: @"PATH=/opt/local/bin:/usr/local/bin:/usr/local/git/bin:/usr/bin; git pull %@ %@", opt, repo];
+	[opt release];
+    
+    NSTask *task  = [[NSTask alloc] init];
+    NSPipe *pipe  = [[NSPipe alloc] init];
+    
+    [task setLaunchPath: @"/bin/sh"];
+    [task setCurrentDirectoryPath: path];
+    [task setArguments: [NSArray arrayWithObjects: @"-c", cmd, nil]];
+    
+    [task setStandardError: pipe];
+    [task launch];
+    
+    NSFileHandle *handle = [pipe fileHandleForReading];
+    NSData *data = [handle  readDataToEndOfFile];
+    NSString *result = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+    
+	[cmd release];
+    [task release];
+    [pipe release];
+    
+    return input;
+}
+
+@end
